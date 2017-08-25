9e79c4472e64940e4113af10a307d96fb22fe0b8
Justin Tran <jctran@princeton.edu>
2017-08-25 00:34:03 -0700

    Update hackerNewsTwitterBot.py

--- hackerNewsTwitterBot.py
+++ hackerNewsTwitterBot.py
@@ -12,7 +12,7 @@ consumer_key = os.environ.get('CONSUMER_KEY')
 consumer_secret = os.environ.get('CONSUMER_SECRET')
 access_token = os.environ.get('ACCESS_TOKEN')
 access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
-
+print(consumer_key)
 # Instantiate Tweepy client with keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
