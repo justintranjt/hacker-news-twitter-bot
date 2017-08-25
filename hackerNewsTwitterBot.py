d0fe8559987ead1f3a1f30c7114a08d3c98a3be7
Justin Tran <jctran@princeton.edu>
2017-08-25 00:39:19 -0700

    Update hackerNewsTwitterBot.py

--- hackerNewsTwitterBot.py
+++ hackerNewsTwitterBot.py
@@ -5,7 +5,6 @@ from hackernews import HackerNews
 from selenium import webdriver
 from PIL import Image
 from io import BytesIO
-import sys
 
 # Personalized Twitter API keys generated for Tweepy use; insert your own
 # Using .env file access through Heroku environment variables
@@ -13,8 +12,7 @@ consumer_key = os.environ.get('CONSUMER_KEY')
 consumer_secret = os.environ.get('CONSUMER_SECRET')
 access_token = os.environ.get('ACCESS_TOKEN')
 access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
-print(consumer_key)
-sys.stdout.flush()
+
 # Instantiate Tweepy client with keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
