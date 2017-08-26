54e055dfde44e95a15efae859ac7de204b2c5c4e
Justin Tran <jctran@princeton.edu>
2017-08-26 12:07:40 -0700

    Update hackerNewsTwitterBot.py

--- hackerNewsTwitterBot.py
+++ hackerNewsTwitterBot.py
@@ -19,53 +19,63 @@ api = tweepy.API(auth)
 
 # Screenshots Hacker News posts and uploads to Twitter as banner
 def refresh_banner():
-	# Using Selenium and PhantomJS browser to screenshot Hacker News posts
-	# phantomJS_path = 'C:\\Python\\selenium\\webdriver\\phantomjs\\phantomjs.exe' LOCAL USE ONLY
-	driver = webdriver.PhantomJS()
-	driver.set_window_size(1600, 900)
-	driver.get('https://news.ycombinator.com')
-	img = driver.get_screenshot_as_png()  # Save screenshot as binary data
+    # Using Selenium and PhantomJS browser to screenshot Hacker News posts
+    # phantomJS_path = 'C:\\Python\\selenium\\webdriver\\phantomjs\\phantomjs.exe' LOCAL USE ONLY
+    driver = webdriver.PhantomJS()
+    driver.set_window_size(1600, 900)
+    driver.get('https://news.ycombinator.com')
+    img = driver.get_screenshot_as_png()  # Save screenshot as binary data
 
-	# Find element on page with posts and get element pixel location on page
-	element = driver.find_element_by_tag_name('tbody')
-	location = element.location
-	size = element.size
-	driver.quit()
+    # Find element on page with posts and get element pixel location on page
+    element = driver.find_element_by_tag_name('tbody')
+    location = element.location
+    size = element.size
+    driver.quit()
 
-	# Load screenshotted image from memory and crop for upload
-	img = Image.open(BytesIO(img))
-	left = location['x']
-	upper = location['y']
-	right = location['x'] + size['width']
-	lower = size['height'] - 777  # Format for Twitter's autocrop
-	img = img.crop((left, upper, right, lower))  # Crop at defined points
-	img.save('screenshot.png')  # Saves cropped image
-	api.update_profile_banner('screenshot.png')  # Uploads cropped banner
+    # Load screenshotted image from memory and crop for upload
+    img = Image.open(BytesIO(img))
+    left = location['x']
+    upper = location['y']
+    right = location['x'] + size['width']
+    lower = size['height'] - 777  # Format for Twitter's autocrop
+    img = img.crop((left, upper, right, lower))  # Crop at defined points
+    img.save('screenshot.png')  # Saves cropped image
+    api.update_profile_banner('screenshot.png')  # Uploads cropped banner
 
 # Tweets top 30 stories on Hacker News with no duplicates on Twitter feed
 def refresh_posts():
-	hn = HackerNews()
-	for story in hn.top_stories(limit=30):  # Only viewing top 30 posts on HN
-		story_id = hn.get_item(story)
+    hn = HackerNews()
+    for story in hn.top_stories(limit=30):  # Only viewing top 30 posts on HN
+        story_id = hn.get_item(story)
 
-		#  Tweets title, story URL, and comments
-		if len(story_id.title) > 76:  # Adjusting for max tweet length
-			story_title = (story_id.title.rsplit(' ', 1)[0] + '\n')
-		else:
-			story_title = (story_id.title + '\n')
+        #  Tweets title, story URL, and comments
+        if len(story_id.title) > 76:  # Adjusting for max tweet length
+            story_title = (story_id.title.rsplit(' ', 1)[0] + '\n')
+        else:
+            story_title = (story_id.title + '\n')
 
-		story_url = ('Link: ' + story_id.url + '\n')
-		story_comments = ('Cmts.: https://news.ycombinator.com/item?id=%s' %
-						  str(story_id.item_id))
-		# If tweet is a duplicate, ignores the post and doesn't tweet
-		try:
-			api.update_status(story_title + story_url + story_comments)
-		except tweepy.error.TweepError:
-			continue
+        story_comments = ('Cmts.: https://news.ycombinator.com/item?id=%s' %
+                          str(story_id.item_id))
+
+        #  Check to see if post has an external link
+        story_url = ('Link: ' + story_id.url + '\n')
+        if story_id.url is None:
+            try: # If tweet is a duplicate, ignores the post and doesn't tweet
+                api.update_status(story_title + story_comments)
+            except tweepy.error.TweepError:
+                continue
+        else:
+            # If tweet is a duplicate, ignores the post and doesn't tweet
+            try:
+                api.update_status(story_title + story_url + story_comments)
+            except tweepy.error.TweepError:
+                continue
+
 
 def main():
-	refresh_banner()  # Refresh banner on Twitter page
-	refresh_posts()  # Tweet new posts on Twitter page
+    refresh_banner()  # Refresh banner on Twitter page
+    refresh_posts()  # Tweet new posts on Twitter page
+
 
 if __name__ == '__main__':
-	main()
+    main()
