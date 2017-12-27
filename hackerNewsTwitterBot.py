3bfc1f83596822657fdbb46f45ae747264e2e2b2
Justin Tran <jctran@princeton.edu>
2017-12-26 22:00:26 -0800

    Disabled extensions

--- hackerNewsTwitterBot.py
+++ hackerNewsTwitterBot.py
@@ -27,6 +27,7 @@ def refresh_banner():
 	chrome_options.add_argument('--window-size=1400, 900')
 	chrome_options.add_argument('--no-sandbox')
 	chrome_options.add_argument('--disable-gpu')
+	chrome_options.add_argument('--disable-extensions')
 	driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=chrome_options)
 	driver.get('https://news.ycombinator.com')
 	img = driver.get_screenshot_as_png()  # Save screenshot as binary data
